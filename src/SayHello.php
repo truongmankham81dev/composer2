91f5858bb284bbd9164c561a0f7d50b3258d4059
truongmankham81dev <38904185+truongmankham81dev@users.noreply.github.com>
2019-02-11 16:59:33 +0700

    Add files via upload

---
+++ src/SayHello.php
@@ -0,0 +1,10 @@
+<?php
+namespace truongmankham81dev\SayHello;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer Test branch!!!!<br />';
+    }
+}
+?>
---
+++ src1/SayHello.php
@@ -0,0 +1,10 @@
+<?php
+namespace truongmankham81dev\SayHello1;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer Test branch!!!!<br />';
+    }
+}
+?>
